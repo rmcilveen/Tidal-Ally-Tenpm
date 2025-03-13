41487b05994baf82f2e96bcc8b514ea2e39cc73f
Bob McIlveen <rmcilveen@apexfintechsolutions.com>
2025-03-13 10:37:10 +0000

    Initial Commit

---
+++ ally_10pm.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+filename = "Ally10pm.html"
+
+def send_email_via_smtp(recipient, subject, body):
+    try:
+        message = MIMEMultipart('alternative')
+        message['From'] = 'Tidal-NoReply@apexclearing.com'
+        message['To'] = recipient
+        message['Subject'] = subject
+        msg = open(filename,"r")
+        #print(msg.read())
+        message.attach(MIMEText(msg.read(), 'html'))
+        #print(message.as_string())
+    
+        # SMTP session
+        session =    smtplib.SMTP('nonauthrelay.apexclearing.local',25)#smtplib.SMTP('smtp.office365.com', 587)  # use outlook's smtp server and port ## Updated to internal relay server
+        #session.starttls()  # enable security
+        #session.login(sender_address, sender_pass)  # login with mail_id and password
+        text = message.as_string()
+        session.sendmail('Tidal-NoReply@apexclearing.com', recipient.split(","), text)
+        session.quit()
+    
+    except Exception as e:
+        print("Error", e)
+
+email_Subject = '*** TEST *** Ally Critical Data Feeds - Batch Update for 10:00pm CT'
+send_email_via_smtp('rmcilveen@apexfintechsolutions.com,arossi@apexfintechsolutions.com, acorales@apexfintechsolutions.com, croaquin@apexfintechsolutions.com, mkulkarni@apexfintechsolutions.com', email_Subject, "") 
